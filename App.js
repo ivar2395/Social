fd327e1afed334b04f8f23441db5e9f32f1f37d0
Ravindranathan M <ravindranathan95@gmail.com>
2021-12-01 15:03:48 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,54 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import OnboardingScreen from './screens/OnboardingScreen';
+import LoginScreen from './screens/LoginScreen';
+import AsyncStorage from 'react-native';
+
+const AppStack = createStackNavigator();
+
+const App = () => {
+  const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
+
+  useEffect(() => {
+    AsyncStorage.getItem('alreadyLaunched').then(value => {
+        if(value == null){
+          AsyncStorage.setItem('alreadyLaunched', 'true');
+          setIsFirstLaunch(true);
+        } else{
+          setIsFirstLaunch(false);
+        }
+    });
+
+  },[]);
+
+  if(isFirstLaunch === null){
+    return null;
+  } else if(isFirstLaunch === true){
+    return(
+      <NavigationContainer>
+        <AppStack.Navigator
+          headerMode="none"
+        >
+          <AppStack.Screen name="Onboarding" component={OnboardingScreen}/>
+          <AppStack.Screen name="Login" component={LoginScreen}/>
+      </AppStack.Navigator>
+      </NavigationContainer>
+
+      );
+  } else{
+    return <LoginScreen />;
+  }
+
+}
+
+export default App;
---
+++ screens/LoginScreen.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet} from 'react-native';
+import Onboarding from 'react-native-onboarding-swiper';
+
+
+const LoginScreen = () => {
+  return(
+    <View style= {styles.container}>
+      <Text>Login Screen</Text>
+      <Button
+        title="Click here"
+        onPress={() => alert('Button Clicked!')}
+      />
+    </View>
+
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+});
